b46f972edd45fae0be7cfdf26c393d8b1173f853
oxozon-o4 <oxozon.o4@gmail.com>
2020-02-23 12:41:13 +0100

    Change

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
+function renderPhoto(photo) {
+    console.log('photo', photo)
+
+    // Create HTMLImageElement
+    const $photo = document.createElement('img');
+    $photo.src = photo;
+    // $photo.setAttribute('src', photo); Diffrent style
+
+    //Render
+    const $photos = document.querySelector('#photos');
+    $photos.append($photo);
+}
+
+function renderPhotos(photos) {
+    photos.forEach(function (photo) {
+        renderPhoto(photo);
+    });
+    
+}
+
 function main() {
     // var let const    const be cant change referance to value
     const photos = [
@@ -5,19 +25,7 @@ function main() {
         "https://i.picsum.photos/id/1035/200/300.jpg",
         "https://i.picsum.photos/id/1033/200/300.jpg",
     ];
-
-    for (let i = 0; i < photos.length; i++) {
-        const photo = photos [i];
-        console.log('photo',i,photo)
-
-        // Create HTMLImageElement
-        const $photo = document.createElement('img');
-        $photo.src = photo;
-        // $photo.setAttribute('src', photo); Diffrent style
-
-        //Render
-        const $photos = document.querySelector('#photos');
-        $photos.append($photo);
-    }
+    renderPhotos(photos);
+    
 }
 main();
