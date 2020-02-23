51a2ba2b1fdb3098b69630e0c39f4f419b7b54ce
oxozon-o4 <oxozon.o4@gmail.com>
2020-02-23 14:11:19 +0100

    Adding If Statement

--- scripts/main.js
+++ scripts/main.js
@@ -17,25 +17,36 @@ function renderPhotos(photos) {
     });
 }
 
-function displayMessage() {
+function displayMessage(message) {
     const $photos = document.querySelector('#photos');
     const $message = document.createElement('div');
 
     $message.classList.add('message'); // for class
     // $message.class.id = message
-   
-    const $header = document.createElement('div');
-    $header.classList.add('message-header');
-    $header.textContent = 'Message to the user';
 
-    const $body = document.createElement('div');
-    $body.classList.add('message-body');
-    $body.textContent = "Gallery is empty. We don't have any photos";
+    // const $header = document.createElement('div');
+    // $header.classList.add('message-header');
+    // $header.textContent = 'Message to the user';
 
+    // const $body = document.createElement('div');
+    // $body.classList.add('message-body');
+    // $body.textContent = "Gallery is empty. We don't have any photos";
 
-    $message.append($header);
-    $message.append($body);
+    // $message.append($header);
+    // $message.append($body);
     $photos.append($message);
+    // Interpolation
+    $message.innerHTML = `
+    <div class="message-header">
+        Message
+    </div>
+    <div class="message-body">
+       ${message}
+    </div>
+    `;
+}
+function displayEmptyGalleryMessage() {
+    displayMessage("Gallery is empty. We don't have any photos");
 }
 
 function main() {
@@ -46,6 +57,13 @@ function main() {
         "https://i.picsum.photos/id/1033/200/300.jpg",
     ];
     //renderPhotos(photos);
-    displayMessage();
+    const isGalleryEmpty = (photos.length === 0);
+
+    if (isGalleryEmpty) {
+        displayEmptyGalleryMessage();
+    }
+    else {
+        renderPhotos(photos);
+    }
 }
 main();
