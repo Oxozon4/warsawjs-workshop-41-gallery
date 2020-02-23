3c4a9fda09ab8c94427a1d4bc1b799ee1aadaf40
oxozon-o4 <oxozon.o4@gmail.com>
2020-02-23 16:09:49 +0100

    Add Clock

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,6 @@ function renderPhoto(photo) {
     const $photos = document.querySelector('#photos');
     $photos.append($photo);
 }
-
 function renderPhotos(photos) {
     photos.forEach(function (photo) {
         try {renderPhoto(photo);
@@ -19,22 +18,17 @@ function renderPhotos(photos) {
         }
     });
 }
-
 function displayMessage(message) {
     const $photos = document.querySelector('#photos');
     const $message = document.createElement('div');
-
     $message.classList.add('message'); // for class
     // $message.class.id = message
-
     // const $header = document.createElement('div');
     // $header.classList.add('message-header');
     // $header.textContent = 'Message to the user';
-
     // const $body = document.createElement('div');
     // $body.classList.add('message-body');
     // $body.textContent = "Gallery is empty. We don't have any photos";
-
     // $message.append($header);
     // $message.append($body);
     $photos.append($message);
@@ -51,35 +45,38 @@ function displayMessage(message) {
 function displayEmptyGalleryMessage() {
     displayMessage("Gallery is empty. We don't have any photos");
 }
-
+function displayCurrentTime() {
+    setInterval(function () {
+        // 1. Get referance to DOM element
+        const $clock = document.querySelector('#current-time');
+        // 2. Get current time from Date Object
+        const date = new Date();
+        const currentDate = date.toLocaleTimeString('pl');
+        // 3. Render time to DOM element
+        $clock.textContent = currentDate;
+    }, 1000)
+}
 function main() {
     loader.show();
-
-
 setTimeout(function () {
-    
     const photos = [
-       { url:  "https://i.picsum.photos/id/1036/200/300.jpg"},
+       { url: "https://i.picsum.photos/id/1036/200/300.jpg"},
        { url: "https://i.picsum.photos/id/1035/200/300.jpg"},
        { url: "https://i.picsum.photos/id/1033/200/300.jpg"},
        null
     ];
     //renderPhotos(photos);
     const isGalleryEmpty = (photos.length === 0);
-
     loader.hide();
-
     if (isGalleryEmpty) {
         displayEmptyGalleryMessage();
     }
     else {
         renderPhotos(photos);
     }
-
 }, 2000);
-
-
+displayCurrentTime();
     // var let const    const be cant change referance to value
-
 }
+
 main();
